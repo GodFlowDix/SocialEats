43581da99a7db9f78e935c7b86647d8a6c356c4b
Mike Dix <godflowdix@gmail.com>
2019-11-04 19:55:56 -0500

    Initial Commit

---
+++ SocialEats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialEats
+//
+//  Created by Mike Dix on 11/4/19.
+//  Copyright © 2019 Mike Dix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
